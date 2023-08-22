95c00397ced6337877883ef85d657663783f94ca
ricky-kiva <ricky.kiva@gmail.com>
2023-08-23 01:48:07 +0700

    feat(presentation-activity): implement Timber to action

--- app/src/main/java/com/rickyslash/reportcrashlyticsapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/reportcrashlyticsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import timber.log.Timber
 import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Timber.d("Test debugging") // log using Timber 'debug'
+
         val btnCrash = findViewById<Button>(R.id.btn_crash)
         btnCrash.setOnClickListener {
             FirebaseCrashlytics.getInstance().log("Clicked on crash button") // add log to crashlytics
             FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data") // send data to crashlytics
-            throw RuntimeException("Test Crash") // imitate throwing exception
+            try {
+                throw RuntimeException("Test Crash") // imitates throwing exception
+            } catch (e: Exception) {
+                Timber.e("Test non fatal exception") // log using Timber 'error'
+            }
         }
     }
 }
