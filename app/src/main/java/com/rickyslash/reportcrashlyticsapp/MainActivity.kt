c3da26bd72ff32c0b6589e445bba54f324598aad
ricky-kiva <ricky.kiva@gmail.com>
2023-08-23 00:52:18 +0700

    feat(crashlytics): imitate exception and add Crashlytics actions

--- app/src/main/java/com/rickyslash/reportcrashlyticsapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/reportcrashlyticsapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.rickyslash.reportcrashlyticsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCrash = findViewById<Button>(R.id.btn_crash)
+        btnCrash.setOnClickListener {
+            FirebaseCrashlytics.getInstance().log("Clicked on crash button") // add log to crashlytics
+            FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data") // send data to crashlytics
+            throw RuntimeException("Test Crash") // imitate throwing exception
+        }
     }
 }
